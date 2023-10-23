23ca0ee4d0d455908774eb2630c7ab6511800124
Crowdbotics <team@crowdbotics.com>
2023-10-23 03:52:59 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "magicpost-44103",
+  url: "https://magicpost-44103.botics.co",
+  api: "https://magicpost-44103.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
